17de41e147f19dfb43302be9a1373cb33359674c
marendu <marendu@qq.com>
2020-05-14 19:35:50 +0800

    s

--- nodeSever/app.js
+++ nodeSever/app.js
@@ -5,18 +5,17 @@
   const koaStaticCache = require('koa-static-cache');
   var cors = require('koa2-cors');
   const app = new Koa()
-  // 定义父路由
   const router = new Router()
   app.use(
     cors({
         origin: function(ctx) { //设置允许来自指定域名请求
-            return '*'; //只允许http://localhost:8080这个域名的请求
+            return '*'; 
         },
-        maxAge: 5, //指定本次预检请求的有效期，单位为秒。
-        credentials: true, //是否允许发送Cookie
-        allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
-        allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
-        exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+        maxAge: 5, 
+        credentials: true, 
+        allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowHeaders: ['Content-Type', 'Authorization', 'Accept'], 
+        exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] 
     })
   );
 
@@ -26,7 +25,7 @@
     gzip: true
   }))
   router.get('/', (ctx) => {
-    console.log(11111);
+    console.log('进入到页面');
     const content = fs.readFileSync('./dist/index.html');
     ctx.body = content.toString();
   })
@@ -34,6 +33,6 @@
   
   // 挂载路由
   app.use(router.routes())
-  app.listen(8083);
+  app.listen(8083); //如果端口占用，这里修改
   console.log('服务器开启');
   })()
